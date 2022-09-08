8f45c617981930c10242de17852c42f8d35b362c
LuqmanHkim <l.hakim070999@gmail.com>
2022-09-08 11:27:11 +0700

    coba

---
+++ pyy.py
@@ -0,0 +1,14 @@
+import pyautogui as pt
+from time import sleep
+import random
+import pandas as pd
+
+
+while True:
+    print(pt.position())
+    pt.scroll(-100)
+    sleep(1)
+
+
+left= 1238,315
+top = 1314,315
