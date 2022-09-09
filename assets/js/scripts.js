c2bfe1328766ab7c106c375a44e3f0a7c10a97fd
LuqmanHkim <l.hakim070999@gmail.com>
2022-09-09 14:35:23 +0700

    eee

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,50 +1,40 @@
-const aa = document.getElementById('footergen')
-let yeard =  new Date().getFullYear();
-ftext = `${aa.innerText[0]} ${yeard} Manhakkim`
-aa.innerText = ftext
+const aa = document.getElementById("footergen");
+let yeard = new Date().getFullYear();
+ftext = `${aa.innerText[0]} ${yeard} Manhakkim`;
+aa.innerText = ftext;
 
-function mailtoo(){
-    const subject = document.getElementById('subjecte').value
-    const body = document.getElementById('bodye').value
-    window.location.href=`mailto:lhakim1762@gmail.com?subject=${subject}&body=${body}`
+function mailtoo() {
+  const subject = document.getElementById("subjecte").value;
+  const body = document.getElementById("bodye").value;
+  window.location.href = `mailto:lhakim1762@gmail.com?subject=${subject}&body=${body}`;
 }
 
-function setact(bScroll){
-    
-}
+function setact(bScroll) {}
 
-window.onscroll = function() {
+window.onscroll = function () {
+  var sections = document.querySelectorAll("section");
+  const navv = document.getElementsByClassName("nav-link");
+  var tamp = [];
 
-    var sections = document.querySelectorAll("section")
-    const navv =document.getElementsByClassName('nav-link')
+  var bScroll = document.documentElement.scrollTop + 2;
+  console.log(`bs = ${bScroll}`);
+  for (var i = 1; i < sections.length; i++) {
+    var sHeight = sections[i].offsetHeight;
+    var offsets = sections[i].offsetTop - 50;
+    console.log(sections[i]);
+    console.log(sHeight, offsets);
 
-    var bScroll = (document.documentElement.scrollTop)+2;
-    for (var i = 1; i < sections.length; i++) {
-  
-  
-      var sHeight = sections[i].offsetHeight;
-      var offsets = sections[i].offsetTop;
-      if (bScroll ) {
-        
-      }
-      if (bScroll > offsets && bScroll < offsets + sHeight) {
-        navv[i-1].className = "nav-link active"
-      } else {
-        navv[i-1].className = "nav-link"
+    tamp.push([sHeight, offsets]);
 
-      }
-
-    // if (bScroll > offsets) {
-    //     navv[i-1].className = "nav-link active"
-    //   } else {
-    //     navv[i-1].className = "nav-link"
-
-    //   }
-  
-  
+    if (bScroll > offsets && bScroll < offsets + sHeight) {
+      navv[i - 1].className = "nav-link active";
+    } else {
+      navv[i - 1].className = "nav-link";
     }
-  
-  
-  
   }
-
+};
+// if (bScroll > offsets && bScroll < offsets + sHeight) {
+//   navv[i-1].className = "nav-link active"
+// } else {
+//   navv[i-1].className = "nav-link"
+// }
