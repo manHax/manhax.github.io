b5e781a932b994fd05a9384c89afd2826ab690b7
LuqmanHkim <l.hakim070999@gmail.com>
2022-09-09 09:43:18 +0700

    mailco

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,9 @@ let yeard =  new Date().getFullYear();
 ftext = `${aa.innerText[0]} ${yeard} Company, Inc`
 aa.innerText = ftext
 
+function mailtoo(){
+    const subject = document.getElementById('subjecte').value
+    const body = document.getElementById('bodye').value
+    window.location.href=`mailto:lhakim1762@gmail.com?subject=${subject}&body=${body}`
+}
+
